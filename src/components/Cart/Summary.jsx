34fdd3629e2000a9e9de3e625184dfaecff948f5
manzonlucas <manzonlucasm@gmail.com>
2023-01-25 12:58:49 -0300

    add summary buttons with MUI components

--- src/components/Cart/Summary.jsx
+++ src/components/Cart/Summary.jsx
@@ -1,7 +1,35 @@
+import { Button } from "@mui/material";
+
 export default function Summary() {
   return (
     <section className="w-1/4">
       <h2 className="text-xl font-bold mr-1">Order summary</h2>
+
+      <div>
+        <h3>Number of items</h3>
+        <span>3</span>
+      </div>
+
+      <div>
+        <h3>Total</h3>
+        <span>$$$</span>
+      </div>
+
+      <div>
+        <Button
+          variant="contained"
+          sx={{
+            color: '', backgroundColor: '#3A4451', textTransform: 'none', "&.MuiButtonBase-root:hover": { bgcolor: "#3A4451" }
+          }} >
+          Proceed to checkout
+        </Button>
+
+        <Button
+          variant="text"
+          sx={{ textTransform: 'none', color: '#3A4451', fontWeight: 'bold', border: '2px solid #3A4451' }}>
+          Continue shopping
+        </Button>
+      </div>
     </section>
   )
 }
